0fb5a08a3a12bbd65597897bb1ee218dea252e1f
aaliya-b <aaliyabs22@gmail.com>
2024-05-26 14:09:10 -0700

    Update WaterRunMain.java

--- Water Run-core/src/com/mygdx/waterrun/WaterRunMain.java
+++ Water Run-core/src/com/mygdx/waterrun/WaterRunMain.java
@@ -1,62 +1,65 @@
 package com.mygdx.waterrun;
 
-import java.util.ArrayList;
-
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.Screen;
+import com.mygdx.waterrun.screens.IntroScreen;
+import com.mygdx.waterrun.screens.GameScreen;
+import com.mygdx.waterrun.screens.MainMenu;
+import com.mygdx.waterrun.screens.GameOverScreen;
 
 public class WaterRunMain extends ApplicationAdapter {
-	SpriteBatch batch;
-    Player player;
-    Texture background;
-    PlatformGenerator platformGenerator;
-    OrthographicCamera camera;
-
+	private GameScreen game;
+	public IntroScreen intro;
+	private GameOverScreen gameover;
+	private MainMenu menu;
+	private Screen screen;
+    
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        player = new Player(100, 300);
-        background = new Texture("background.png");
+    	game = new GameScreen(this);
+        gameover = new GameOverScreen(this);
+        intro = new IntroScreen(this);
+        menu = new MainMenu(this);
         
-        platformGenerator = new PlatformGenerator(player);
-        
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        setScreen(intro);
     }
-
+    
+    public void setScreen(Screen screen) {
+    	if (this.screen != null) {
+            this.screen.hide();
+        }
+        this.screen = screen;
+        if (this.screen != null) {
+            this.screen.show();
+        }
+    }
+    
     @Override
     public void render() {
-        player.handleInput();
-        player.update();
-
-        // Update camera position to follow the player
-        camera.position.x = player.getX() + Gdx.graphics.getWidth() / 4; // Adjust as needed
-        camera.update();
-        
-        platformGenerator.update();
-        platformGenerator.checkCollisions(player);
-        
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.setProjectionMatrix(camera.combined);
-        batch.begin();
-        batch.draw(background, camera.position.x - camera.viewportWidth / 2, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        player.render(batch);
-        platformGenerator.render(batch);
-        batch.end();
+    	if (screen != null) {
+            screen.render(Gdx.graphics.getDeltaTime());
+        }
     }
 
+    public void gameOver() {
+        setScreen(gameover);
+    }
+    
     @Override
     public void dispose() {
-        batch.dispose();
-        background.dispose();
-        player.dispose();
-        platformGenerator.dispose();
+    	if (intro != null) {
+            intro.dispose();
+        }
+        if (game != null) {
+            game.dispose();
+        }
+        if (gameover != null) {
+            gameover.dispose();
+        }
+        if (menu != null)
+        {
+        	menu.dispose();
+        }
     }
 }
