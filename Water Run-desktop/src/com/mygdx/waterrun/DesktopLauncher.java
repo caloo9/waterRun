30b7c13b5c25a705672945a631f864b69cc9ba54
aaliya-b <aaliyabs22@gmail.com>
2024-05-26 14:10:31 -0700

    Update DesktopLauncher.java

--- Water Run-desktop/src/com/mygdx/waterrun/DesktopLauncher.java
+++ Water Run-desktop/src/com/mygdx/waterrun/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.mygdx.waterrun;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.mygdx.waterrun.WaterRunMain;
@@ -9,7 +10,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setTitle("Water Run");
-		config.setWindowedMode(1000,900);
+		config.setWindowedMode(1500, 1000);
 		config.useVsync(true);
 		config.setForegroundFPS(60);
 		new Lwjgl3Application(new WaterRunMain(), config);
