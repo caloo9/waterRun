1db2f9f7e44e8a9db10a1728f287d38062e19ea1
aaliya-b <aaliyabs22@gmail.com>
2024-05-29 23:25:34 -0700

    Update GameOverScreen.java

--- Water Run-core/src/com/mygdx/waterrun/screens/GameOverScreen.java
+++ Water Run-core/src/com/mygdx/waterrun/screens/GameOverScreen.java
@@ -27,7 +27,13 @@ public class GameOverScreen extends WaterRunScreens{
 		over = new TextureRegion(overTexture);
 		batch = new SpriteBatch();
 		camera = new OrthographicCamera();
-        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        	camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        
+        	FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("Water-Regular.otf")); 
+        	FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        	parameter.size = 62;
+        	font = generator.generateFont(parameter);
+        	generator.dispose(); // Dispose to avoid memory leaks
 	}
 
 	@Override
@@ -37,6 +43,30 @@ public class GameOverScreen extends WaterRunScreens{
 		batch.draw(over, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.end();
 
+		// Draw the score
+		String scoreText = "Final Score: " + finalScore;
+        float textWidth = font.getRegion().getRegionWidth();
+        float x = (Gdx.graphics.getWidth()) / 2 - 170;
+        float y = Gdx.graphics.getHeight() / 2 - 250;
+        
+        if (finalScore > 9)
+        {
+        font.draw(batch, scoreText, x, y);
+        }
+        else if (finalScore > 99)
+        {
+        	font.draw(batch, scoreText, x-200, y);
+        }
+        else if (finalScore > 999)
+        {
+        	font.draw(batch, scoreText, x-230, y);
+        }
+        else if(finalScore > 9999)
+        {
+        	font.draw(batch, scoreText, x-260, y);
+        }
+		batch.end();
+		
 		time += delta;
 		if (time > 1) {
 			if (Gdx.input.isKeyPressed(Keys.SPACE)) {
@@ -47,7 +77,7 @@ public class GameOverScreen extends WaterRunScreens{
 
 	@Override
 	public void hide () {
-		Gdx.app.debug("Cubocy", "dispose intro");
+		Gdx.app.debug("WaterRun", "dispose intro");
 		batch.dispose();
 		over.getTexture().dispose();
 	}
